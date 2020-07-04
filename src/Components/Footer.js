04e3b3cf082cab62e4f9589eb53e8102abcb477b
websudo <ritiknair19@gmail.com>
2020-07-04 11:52:20 +0530

    Footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 import './Footer.css';
+import flogo from '../images/facebook.png'
+import ilogo from '../images/instagram.png'
+import tlogo from '../images/twitter.png'
+import plogo from '../images/pinterest.png'
+import llogo from '../images/license.png'
+
 
 
 export class Footer extends Component {
@@ -11,20 +17,20 @@ export class Footer extends Component {
                 <div className="container">
                 <div className="row">
                 <div className="col-3">
-                <a href="#" target="_blank"><img src="/images/facebook.png" alt="" width="45px" height="45px"/></a>
+                <a href="#" target="_blank"><img src={flogo} alt="..." width="45px" height="45px"/></a>
                 </div>
                 <div className="col-3">
-                <a href="#" target="_blank" ><img src="/images/instagram.png" alt="" width="45px" height="45px"/></a>
+                <a href="#" target="_blank" ><img src={ilogo} alt="..." width="45px" height="45px"/></a>
                 </div>
                 <div className="col-3">
-                <a href="#" target="_blank" ><img src="/images/twitter.png" alt="" width="45px" height="45px"/></a>
+                <a href="#" target="_blank" ><img src={tlogo} alt="...." width="45px" height="45px"/></a>
                 </div>
                 <div className="col-3">
-                <a href="#"><img src="/images/pinterest.png"  alt="" width="45px" height="45px"/></a> 
+                <a href="#"><img src={plogo}  alt="..." width="45px" height="45px"/></a> 
                 </div>
                 </div><br></br>
                 <div className="row justify-content-center">
-                <img src="/images/license.png" alt="" width="20px" height="20px" />
+                <img src={llogo} alt="..." width="20px" height="20px" />
                 <p>2020 TVMaze LLC All rights reserved.</p>
                 </div>
                 </div>
