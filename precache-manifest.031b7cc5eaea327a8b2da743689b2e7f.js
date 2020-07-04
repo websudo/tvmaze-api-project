d0416e1ff45dbbeaa1d054d39cd9c5918a03a509
websudo <ritiknair19@gmail.com>
2020-07-04 08:33:54 +0530

    Updates

---
+++ precache-manifest.031b7cc5eaea327a8b2da743689b2e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4567a3f6be894dc358c349cbcd040ac8",
+    "url": "/tvmaze-api-project/index.html"
+  },
+  {
+    "revision": "c8172a7e6352e9a2d416",
+    "url": "/tvmaze-api-project/static/css/main.724097c6.chunk.css"
+  },
+  {
+    "revision": "f39ac58d6b650aff58e5",
+    "url": "/tvmaze-api-project/static/js/2.83e579e2.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/tvmaze-api-project/static/js/2.83e579e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8172a7e6352e9a2d416",
+    "url": "/tvmaze-api-project/static/js/main.61240f8e.chunk.js"
+  },
+  {
+    "revision": "5e75c76c2fd67a26028f",
+    "url": "/tvmaze-api-project/static/js/runtime-main.87583dcb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tvmaze-api-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
